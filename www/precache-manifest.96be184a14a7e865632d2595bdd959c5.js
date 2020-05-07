7f6f7488b4bbd0216620446e17109a1a10ed7995
leth2 <happynsad@gmail.com>
2020-05-08 05:38:54 +0900

    init

---
+++ www/precache-manifest.96be184a14a7e865632d2595bdd959c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa96de9edb60ff4da896372fdfe810d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "091d02c288df3658dd76",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "41556e2b90d04abee850",
+    "url": "./static/js/2.a0502e46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a0502e46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "091d02c288df3658dd76",
+    "url": "./static/js/main.e754e59a.chunk.js"
+  },
+  {
+    "revision": "ee33b387cb82a3d1361a",
+    "url": "./static/js/runtime-main.e52042a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
